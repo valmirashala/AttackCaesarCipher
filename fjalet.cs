b4fe5468c8fbea016747a4d0afe9723bb1dc2eaa
Valmira Shala <valmirashalaa@gmail.com>
2022-04-16 22:58:05 +0200

    Create fjalet.cs
    created fjaletkontrolluese

---
+++ fjalet.cs
@@ -0,0 +1,10 @@
+﻿ciphertext = ciphertext.ToUpper();
+
+char[] alfabeti = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                                        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+
+char[] mesazhiEch = ciphertext.ToCharArray();
+string[] tekstiKontrollues = new string[] { "EDHE", "ESHTE" };
+
+string[] Split_Mesazhi = ciphertext.Split(' ');
