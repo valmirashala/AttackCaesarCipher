dfdbd744e9e892ca98fba5252da7be874a03374c
Valmira Shala <valmirashalaa@gmail.com>
2022-04-17 17:25:25 +0200

    Created main function
    final

---
+++ attackcezar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace detyra  
+{
+    class Program // klas
+    {
+        static void Main(string[] args)  
+        {
+            string ciphertext = File.ReadAllText(@"C:\Users\User\Desktop\valmira\fiek\semestri4\Siguri e te dhenave\Projekti2\ciphertext.txt");
+
+            string plaintext = Dekriptimi(ciphertext);  
+            Console.WriteLine("Teksti i dekriptuar:  " + plaintext);   
+
+            Console.ReadKey();  
+        }
+        public static string sms;   
+
+    }
+
+}
+
